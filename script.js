757af57d841487115631df5313efe3fefa977295
Valou <116378179+A1oneeee@users.noreply.github.com>
2024-01-08 11:53:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const toogleBtn = document.querySelector('.toggle_btn');
-const toogleBtnIcon = document.querySelector('.toggle_btn i');
-const dropDownMenu = document.querySelector('.dropdown_menu');
 
 toogleBtn.addEventListener('click', () => {
 	dropDownMenu.classList.toggle('open');
