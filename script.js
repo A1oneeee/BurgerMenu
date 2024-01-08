d86eca0657ea86dc0e572a7dd5ab737fcd26f978
Valou <116378179+A1oneeee@users.noreply.github.com>
2024-01-08 13:01:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const toogleBtn = document.querySelector('.toggle_btn');
+const dropDownMenu = document.querySelector('.dropdown_menu');
+const icon = document.querySelector('#icon');
 
 toogleBtn.addEventListener('click', () => {
 	dropDownMenu.classList.toggle('open');
+	icon.name = (icon.name === 'menu' ? 'close' : 'menu');
 });
